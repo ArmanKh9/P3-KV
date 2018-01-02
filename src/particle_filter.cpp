1efd30e15bdb16055d334346dedc52d2895a0517
ArmanKh9 <khalighi.ar9@gmail.com>
2018-01-02 00:30:15 -0500

    4

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -19,7 +19,7 @@
 
 using namespace std;
 random_device rd;
-default_random_engine gen(rd());
+default_random_engine gen;
 
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
